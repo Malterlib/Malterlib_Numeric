76f92c6c3c4514a6aa3bcd31ed0e3c3397324075
Erik Olofsson <erik@olofsson.info>
2026-01-31 17:18:43 +0100

    Fix TCVec and add tests

--- Source/Malterlib_Numeric_Vector.h
+++ Source/Malterlib_Numeric_Vector.h
@@ -6,243 +6,54 @@
 namespace NMib::NNumeric
 {
 	template <typename t_CType, mint t_nDim>
-	class TCVecAggregate
+	struct TCVec;
+
+	namespace NPrivate
 	{
-	public:
-		using CType = t_CType;
-
-		enum
+		template <typename t_CType>
+		struct TCIsTCVec
 		{
-			ENumDim = t_nDim
+			constexpr static bool mc_Value = false;
 		};
-		CType m_Axis[ENumDim];
-		inline_small const CType &operator [] (mint _iOpr) const
+
+		template <typename t_CType, mint t_nDim>
+		struct TCIsTCVec<TCVec<t_CType, t_nDim>>
 		{
-			return m_Axis[_iOpr];
-		}
+			constexpr static bool mc_Value = true;
+		};
+	}
 
-		inline_small CType &operator [] (mint _iOpr)
-		{
-			return m_Axis[_iOpr];
-		}
-
-		template <typename t_CType1, mint t_nDim1>
-		TCVecAggregate &operator = (const TCVecAggregate<t_CType1, t_nDim1> &_Src) const
-		{
-			mint nDimMin = fg_Min(ENumDim, _Src.ENumDim);
-			for (mint i = 0; i < nDimMin; ++i)
-			{
-				(*this)[i] = _Src[i];
-			}
-			for (mint i = ENumDim; i < _Src.ENumDim; ++i)
-			{
-				(*this)[i] = 0;
-			}
-
-			return *this;
-		}
-
-	};
+	template <typename t_CType>
+	concept cIsTCVec = NPrivate::TCIsTCVec<NTraits::TCRemoveReferenceAndQualifiers<t_CType>>::mc_Value;
 
 	template <typename t_CType, mint t_nDim>
-	class TCVec : public TCVecAggregate<t_CType, t_nDim>
+	struct TCVec
 	{
-	public:
-		using CSuper = TCVecAggregate<t_CType, t_nDim>;
-		using CType = typename CSuper::CType;
+		using CType = t_CType;
 
-		TCVec()
-		{
-		}
-		TCVec(CType _Value[t_nDim])
-		{
-			for (mint i = 0; i < t_nDim; ++i)
-			{
-				(*this)[i] = _Value[i];
-			}
-		}
-		template <typename t_CType1, mint t_nDim1>
-		TCVec(const TCVecAggregate<t_CType1, t_nDim1> &_Src)
-		{
-			mint nDimMin = fg_Min(CSuper::ENumDim, _Src.ENumDim);
-			for (mint i = 0; i < nDimMin; ++i)
-			{
-				(*this)[i] = _Src[i];
-			}
-			for (mint i = CSuper::ENumDim; i <TCVecAggregate<t_CType1, t_nDim1>::ENumDim; ++i)
-			{
-				(*this)[i] = 0;
-			}
-		}
+		constexpr TCVec() noexcept = default;
+		constexpr TCVec(TCVec const &_Src) noexcept = default;
+		constexpr TCVec(TCVec &&_Src) noexcept = default;
+		constexpr TCVec &operator = (TCVec const &_Src) noexcept = default;
+		constexpr TCVec &operator = (TCVec &&_Src) noexcept = default;
 
-		TCVec(const TCVecAggregate<t_CType, t_nDim> &_Src)
-		{
-			*((TCVecAggregate<t_CType, t_nDim> *) this) = _Src;
-		}
-	};
+		template <typename tf_CType, mint tf_nDim>
+		constexpr TCVec(TCVec<tf_CType, tf_nDim> const &_Src);
 
-	template <typename t_CType>
-	class TCVec<t_CType, 2> : public TCVecAggregate<t_CType, 2>
-	{
-	public:
-		using CSuper = TCVecAggregate<t_CType, 2>;
-		using CType = typename CSuper::CType;
+		template <typename ...tfp_CParmas>
+			requires (sizeof...(tfp_CParmas) > 1) || ((!cIsTCVec<tfp_CParmas>) && ...)
+		constexpr TCVec(tfp_CParmas && ...p_Param) noexcept;
 
-		enum
-		{
-			ENumDim = CSuper::ENumDim
-		};
+		constexpr TCVec(CType _Value[t_nDim]) noexcept;
 
-		TCVec()
-		{
-		}
-		TCVec(CType _Value[ENumDim])
-		{
-			(*this)[0] = _Value[0];
-			(*this)[1] = _Value[1];
-		}
-		TCVec(CType _Value0)
-		{
-			(*this)[0] = _Value0;
-			(*this)[1] = _Value0;
-		}
-		TCVec(CType _Value0, CType _Value1)
-		{
-			(*this)[0] = _Value0;
-			(*this)[1] = _Value1;
-		}
-		template <typename t_CType1, mint t_nDim1>
-		TCVec(const TCVecAggregate<t_CType1, t_nDim1> &_Src)
-		{
-			mint nDimMin = fg_Min(ENumDim, _Src.ENumDim);
-			for (mint i = 0; i < nDimMin; ++i)
-			{
-				(*this)[i] = _Src[i];
-			}
-			for (mint i = ENumDim; i < _Src.ENumDim; ++i)
-			{
-				(*this)[i] = 0;
-			}
-		}
+		constexpr inline_small CType const &operator [] (mint _iOpr) const;
+		constexpr inline_small CType &operator [] (mint _iOpr);
 
-		TCVec(const TCVecAggregate<t_CType, ENumDim> &_Src)
-		{
-			*((TCVecAggregate<t_CType, ENumDim> *) this) = _Src;
-		}
-	};
+		template <typename tf_CType, mint tf_nDim>
+		constexpr TCVec &operator = (TCVec<tf_CType, tf_nDim> const &_Src);
 
-	template <typename t_CType>
-	class TCVec<t_CType, 3> : public TCVecAggregate<t_CType, 3>
-	{
-	public:
-		using CSuper = TCVecAggregate<t_CType, 3>;
-		using CType = typename CSuper::CType;
-
-		enum
-		{
-			ENumDim = CSuper::ENumDim
-		};
-
-		TCVec()
-		{
-		}
-		TCVec(CType _Value[ENumDim])
-		{
-			(*this)[0] = _Value[0];
-			(*this)[1] = _Value[1];
-			(*this)[2] = _Value[2];
-		}
-		TCVec(CType _Value0)
-		{
-			(*this)[0] = _Value0;
-			(*this)[1] = _Value0;
-			(*this)[2] = _Value0;
-		}
-		TCVec(CType _Value0, CType _Value1, CType _Value2)
-		{
-			(*this)[0] = _Value0;
-			(*this)[1] = _Value1;
-			(*this)[2] = _Value2;
-		}
-		template <typename t_CType1, mint t_nDim1>
-		TCVec(const TCVecAggregate<t_CType1, t_nDim1> &_Src)
-		{
-			mint nDimMin = fg_Min(ENumDim, _Src.ENumDim);
-			for (mint i = 0; i < nDimMin; ++i)
-			{
-				(*this)[i] = _Src[i];
-			}
-			for (mint i = ENumDim; i < _Src.ENumDim; ++i)
-			{
-				(*this)[i] = 0;
-			}
-		}
-		TCVec(const TCVecAggregate<t_CType, ENumDim> &_Src)
-		{
-			*((TCVecAggregate<t_CType, ENumDim> *) this) = _Src;
-		}
-	};
-
-	template <typename t_CType>
-	class TCVec<t_CType, 4> : public TCVecAggregate<t_CType, 4>
-	{
-	public:
-		using CSuper = TCVecAggregate<t_CType, 4>;
-		using CType = typename CSuper::CType;
-
-		enum
-		{
-			ENumDim = CSuper::ENumDim
-		};
-
-		TCVec()
-		{
-		}
-		TCVec(CType _Value[ENumDim])
-		{
-			(*this)[0] = _Value[0];
-			(*this)[1] = _Value[1];
-			(*this)[2] = _Value[2];
-			(*this)[3] = _Value[3];
-		}
-		TCVec(CType _Value0)
-		{
-			(*this)[0] = _Value0;
-			(*this)[1] = _Value0;
-			(*this)[2] = _Value0;
-			(*this)[4] = _Value0;
-		}
-		TCVec(CType _Value0, CType _Value1, CType _Value2, CType _Value3)
-		{
-			(*this) = TCVecAggregate<t_CType, 4>::fs_Init(_Value0, _Value1, _Value2, _Value3);
-		}
-		template <typename t_CType1, mint t_nDim1>
-		TCVec(const TCVecAggregate<t_CType1, t_nDim1> &_Src)
-		{
-			mint nDimMin = fg_Min(ENumDim, _Src.ENumDim);
-			for (mint i = 0; i < nDimMin; ++i)
-			{
-				(*this)[i] = _Src[i];
-			}
-			for (mint i = ENumDim; i < _Src.ENumDim; ++i)
-			{
-				(*this)[i] = 0;
-			}
-		}
-		TCVec(const TCVecAggregate<t_CType, ENumDim> &_Src)
-		{
-			(*this)[0] = _Src[0];
-			(*this)[1] = _Src[1];
-			(*this)[2] = _Src[2];
-			(*this)[3] = _Src[3];
-		}
-		TCVec(const TCVec &_Src)
-		{
-			(*this)[0] = _Src[0];
-			(*this)[1] = _Src[1];
-			(*this)[2] = _Src[2];
-			(*this)[3] = _Src[3];
-		}
+		constexpr static mint mc_NumDim = t_nDim;
+		CType m_Axis[t_nDim];
 	};
 
 	using CVec2Dint32 = TCVec<int32, 2>;
@@ -256,162 +67,9 @@ namespace NMib::NNumeric
 	using CVec2Dfp32 = TCVec<fp32, 2>;
 	using CVec3Dfp32 = TCVec<fp32, 3>;
 	using CVec4Dfp32 = TCVec<fp32, 4>;
-
-	using CVecAggr2Dint32 = TCVecAggregate<int32, 2>;
-	using CVecAggr3Dint32 = TCVecAggregate<int32, 3>;
-	using CVecAggr4Dint32 = TCVecAggregate<int32, 4>;
-
-	using CVecAggr2Duint32 = TCVecAggregate<int32, 2>;
-	using CVecAggr3Duint32 = TCVecAggregate<int32, 3>;
-	using CVecAggr4Duint32 = TCVecAggregate<int32, 4>;
-
-	using CVecAggr2Dfp32 = TCVecAggregate<fp32, 2>;
-	using CVecAggr3Dfp32 = TCVecAggregate<fp32, 3>;
-	using CVecAggr4Dfp32 = TCVecAggregate<fp32, 4>;
-
-	template <typename t_CType, mint t_nDimX>
-	class TCMatrixAggregate
-	{
-	public:
-		using CVec = t_CType;
-
-		enum
-		{
-			ENumDimY = t_CType::ENumDim,
-			ENumDimX = t_nDimX
-		};
-		CVec m_Columns[ENumDimX];
-
-		inline_small const CVec &operator [] (mint _iOpr) const
-		{
-			return m_Columns[_iOpr];
-		}
-
-		inline_small CVec &operator [] (mint _iOpr)
-		{
-			return m_Columns[_iOpr];
-		}
-
-
-		template <typename t_CType0, mint t_nDimX0, typename t_CType1, mint t_nDimX1>
-		static void fs_Multiply(TCMatrixAggregate<t_CType, t_nDimX> &_Dest, const TCMatrixAggregate<t_CType0, t_nDimX0> &_Src0, const TCMatrixAggregate<t_CType1, t_nDimX1> &_Src1)
-		{
-			enum
-			{
-				EDimX0 = TCMatrixAggregate<t_CType0, t_nDimX0>::ENumDimX,
-				EDimY0 = TCMatrixAggregate<t_CType0, t_nDimX0>::ENumDimY,
-				EDimX1 = TCMatrixAggregate<t_CType1, t_nDimX1>::ENumDimX,
-				EDimY1 = TCMatrixAggregate<t_CType1, t_nDimX1>::ENumDimY,
-				EMaxX = EDimX0 > EDimX1 ? (EDimX0 > ENumDimX ? EDimX0 : ENumDimX) : (EDimX1 > ENumDimX ? EDimX1 : ENumDimX),
-				EMaxY = EDimY0 > EDimY1 ? (EDimY0 > ENumDimY ? EDimY0 : ENumDimY) : (EDimY1 > ENumDimY ? EDimY1 : ENumDimY),
-				EMinX = EDimX0 < EDimX1 ? (EDimX0 < ENumDimX ? EDimX0 : ENumDimX) : (EDimX1 < ENumDimX ? EDimX1 : ENumDimX),
-				EMinY = EDimY0 < EDimY1 ? (EDimY0 < ENumDimY ? EDimY0 : ENumDimY) : (EDimY1 < ENumDimY ? EDimY1 : ENumDimY),
-				EMax = EMaxX > EMaxY ? EMaxX : EMaxY,
-				EMin = EMinX < EMinY ? EMinX : EMinY,
-			};
-			for (mint x = 0; x < ENumDimX; ++x)
-			{
-				for (mint y = 0; y < ENumDimY; ++y)
-				{
-					typename CVec::CType Add = 0;
-					for (mint i = 0; i < EMin; ++i)
-					{
-						Add += _Src0.m_Columns[x][i] * _Src1.m_Columns[i][y];
-					}
-					for (mint i = EMin; i < EDimX1; ++i)
-					{
-						Add += _Src1.m_Columns[i][y];
-					}
-					for (mint i = EMin; i < EDimY0; ++i)
-					{
-						Add += _Src0.m_Columns[x][i];
-					}
-					_Dest.m_Columns[x][y] = Add;
-				}
-			}
-		}
-
-		template <typename t_CType0, mint t_nDimX0>
-		inline_small TCMatrixAggregate operator *(const TCMatrixAggregate<t_CType0, t_nDimX0> &_Src) const
-		{
-			TCMatrixAggregate Ret;
-			fs_Multiply(Ret, *this, _Src);
-			return Ret;
-		}
-	};
-
-	template <>
-	class TCMatrixAggregate<CVecAggr4Dfp32, 4>
-	{
-	public:
-		using CVec = CVecAggr4Dfp32;
-		enum
-		{
-			ENumDimY = CVecAggr4Dfp32::ENumDim,
-			ENumDimX = 4
-		};
-		CVec m_Columns[ENumDimX];
-
-		inline_small const CVec &operator [] (mint _iOpr) const
-		{
-			return m_Columns[_iOpr];
-		}
-
-		inline_small CVec &operator [] (mint _iOpr)
-		{
-			return m_Columns[_iOpr];
-		}
-
-		template <typename t_CType0, mint t_nDimX0, typename t_CType1, mint t_nDimX1>
-		static void fs_Multiply(TCMatrixAggregate<CVec, ENumDimX> &_Dest, const TCMatrixAggregate<t_CType0, t_nDimX0> &_Src0, const TCMatrixAggregate<t_CType1, t_nDimX1> &_Src1)
-		{
-			enum
-			{
-				EDimX0 = TCMatrixAggregate<t_CType0, t_nDimX0>::ENumDimX,
-				EDimY0 = TCMatrixAggregate<t_CType0, t_nDimX0>::ENumDimY,
-				EDimX1 = TCMatrixAggregate<t_CType1, t_nDimX1>::ENumDimX,
-				EDimY1 = TCMatrixAggregate<t_CType1, t_nDimX1>::ENumDimY,
-				EMaxX = EDimX0 > EDimX1 ? (EDimX0 > ENumDimX ? EDimX0 : ENumDimX) : (EDimX1 > ENumDimX ? EDimX1 : ENumDimX),
-				EMaxY = EDimY0 > EDimY1 ? (EDimY0 > ENumDimY ? EDimY0 : ENumDimY) : (EDimY1 > ENumDimY ? EDimY1 : ENumDimY),
-				EMinX = EDimX0 < EDimX1 ? (EDimX0 < ENumDimX ? EDimX0 : ENumDimX) : (EDimX1 < ENumDimX ? EDimX1 : ENumDimX),
-				EMinY = EDimY0 < EDimY1 ? (EDimY0 < ENumDimY ? EDimY0 : ENumDimY) : (EDimY1 < ENumDimY ? EDimY1 : ENumDimY),
-				EMax = EMaxX > EMaxY ? EMaxX : EMaxY,
-				EMin = EMinX < EMinY ? EMinX : EMinY,
-			};
-			for (mint x = 0; x < ENumDimX; ++x)
-			{
-				for (mint y = 0; y < ENumDimY; ++y)
-				{
-					CVec::CType Add = 0;
-					for (mint i = 0; i < EMin; ++i)
-					{
-						Add += _Src0.m_Columns[x][i] * _Src1.m_Columns[i][y];
-					}
-					for (mint i = EMin; i < EDimX1; ++i)
-					{
-						Add += _Src1.m_Columns[i][y];
-					}
-					for (mint i = EMin; i < EDimY0; ++i)
-					{
-						Add += _Src0.m_Columns[x][i];
-					}
-					_Dest.m_Columns[x][y] = Add;
-				}
-			}
-		}
-
-		template <typename t_CType0, mint t_nDimX0>
-		inline_small TCMatrixAggregate operator *(const TCMatrixAggregate<t_CType0, t_nDimX0> &_Src) const
-		{
-			TCMatrixAggregate Ret;
-			fs_Multiply(Ret, *this, _Src);
-			return Ret;
-		}
-	};
-
-	using CVecAggr4x4fp32 = TCMatrixAggregate<CVecAggr4Dfp32, 4>;
-	using CVecAggr3x4fp32 = TCMatrixAggregate<CVecAggr4Dfp32, 3>;
 }
+
+#include "Malterlib_Numeric_Vector.hpp"
 
 #ifndef DMibPNoShortCuts
 	using namespace NMib::NNumeric;
--- Test/Test_Malterlib_Numeric_Vector.cpp
+++ Test/Test_Malterlib_Numeric_Vector.cpp
@@ -3,6 +3,168 @@
 
 #include <Mib/Numeric/Vector>
 
-NMib::NNumeric::CVecAggr2Dfp32 g_TestAggreate2D = {0.0f, 1.0f};
-NMib::NNumeric::CVecAggr3Dfp32 g_TestAggreate3D = {0.0f, 1.0f, 2.0f};
-NMib::NNumeric::CVecAggr4Dfp32 g_TestAggreate4D = {0.0f, 1.0f, 2.0f, 3.0f};
+using namespace NMib::NNumeric;
+using namespace NMib::NStr;
+
+// Compile-time construction tests
+constexpr static CVec2Dfp32 gc_TestAggregate2D{0.0f, 1.0f};
+constexpr static CVec3Dfp32 gc_TestAggregate3D{0.0f, 1.0f, 2.0f};
+constexpr static CVec4Dfp32 gc_TestAggregate4D{0.0f, 1.0f, 2.0f, 3.0f};
+
+// Compile-time access tests
+static_assert(gc_TestAggregate2D[0].f_Get() == 0.0f);
+static_assert(gc_TestAggregate2D[1].f_Get() == 1.0f);
+static_assert(gc_TestAggregate3D[2].f_Get() == 2.0f);
+static_assert(gc_TestAggregate4D[3].f_Get() == 3.0f);
+
+namespace
+{
+	struct CVec_Tests : public NMib::NTest::CTest
+	{
+		void f_DoTests()
+		{
+			DMibTestSuite("Construction")
+			{
+				{
+					DMibTestPath("Default");
+					TCVec<int32, 3> Vec;
+					// Default construction - values are uninitialized, just verify it compiles
+					(void)Vec;
+				}
+
+				{
+					DMibTestPath("Variadic");
+					CVec3Dfp32 Vec(1.0f, 2.0f, 3.0f);
+					DMibExpect(Vec[0], ==, 1.0f);
+					DMibExpect(Vec[1], ==, 2.0f);
+					DMibExpect(Vec[2], ==, 3.0f);
+				}
+
+				{
+					DMibTestPath("Variadic/MixedTypes");
+					// Construct with different numeric types
+					TCVec<fp64, 3> Vec(1, 2.0f, 3.0);
+					DMibExpect(Vec[0], ==, 1.0);
+					DMibExpect(Vec[1], ==, 2.0);
+					DMibExpect(Vec[2], ==, 3.0);
+				}
+			};
+
+			DMibTestSuite("ConvertingConstruction")
+			{
+				{
+					DMibTestPath("SameDim/TypeConversion");
+					CVec3Dfp32 SrcFloat(1.5f, 2.5f, 3.5f);
+					TCVec<int32, 3> DstInt(SrcFloat);
+					DMibExpect(DstInt[0], ==, 1);
+					DMibExpect(DstInt[1], ==, 2);
+					DMibExpect(DstInt[2], ==, 3);
+				}
+
+				{
+					DMibTestPath("SmallerToLarger");
+					CVec2Dfp32 Src2D(1.0f, 2.0f);
+					CVec4Dfp32 Dst4D(Src2D);
+					DMibExpect(Dst4D[0], ==, 1.0f);
+					DMibExpect(Dst4D[1], ==, 2.0f);
+					DMibExpect(Dst4D[2], ==, 0.0f);
+					DMibExpect(Dst4D[3], ==, 0.0f);
+				}
+
+				{
+					DMibTestPath("LargerToSmaller");
+					CVec4Dfp32 Src4D(1.0f, 2.0f, 3.0f, 4.0f);
+					CVec2Dfp32 Dst2D(Src4D);
+					DMibExpect(Dst2D[0], ==, 1.0f);
+					DMibExpect(Dst2D[1], ==, 2.0f);
+				}
+
+				{
+					DMibTestPath("TypeAndDimConversion");
+					TCVec<fp64, 4> SrcDouble(1.5, 2.5, 3.5, 4.5);
+					TCVec<int32, 2> DstInt(SrcDouble);
+					DMibExpect(DstInt[0], ==, 1);
+					DMibExpect(DstInt[1], ==, 2);
+				}
+			};
+
+			DMibTestSuite("IndexAccess")
+			{
+				{
+					DMibTestPath("Read");
+					CVec3Dfp32 const Vec(1.0f, 2.0f, 3.0f);
+					DMibExpect(Vec[0], ==, 1.0f);
+					DMibExpect(Vec[1], ==, 2.0f);
+					DMibExpect(Vec[2], ==, 3.0f);
+				}
+
+				{
+					DMibTestPath("Write");
+					CVec3Dfp32 Vec(0.0f, 0.0f, 0.0f);
+					Vec[0] = 10.0f;
+					Vec[1] = 20.0f;
+					Vec[2] = 30.0f;
+					DMibExpect(Vec[0], ==, 10.0f);
+					DMibExpect(Vec[1], ==, 20.0f);
+					DMibExpect(Vec[2], ==, 30.0f);
+				}
+			};
+
+			DMibTestSuite("Assignment")
+			{
+				{
+					DMibTestPath("SameDim/TypeConversion");
+					CVec3Dfp32 SrcFloat(1.5f, 2.5f, 3.5f);
+					TCVec<int32, 3> DstInt;
+					DstInt = SrcFloat;
+					DMibExpect(DstInt[0], ==, 1);
+					DMibExpect(DstInt[1], ==, 2);
+					DMibExpect(DstInt[2], ==, 3);
+				}
+
+				{
+					DMibTestPath("SmallerToLarger");
+					CVec2Dfp32 Src2D(5.0f, 6.0f);
+					CVec4Dfp32 Dst4D(1.0f, 2.0f, 3.0f, 4.0f);
+					Dst4D = Src2D;
+					DMibExpect(Dst4D[0], ==, 5.0f);
+					DMibExpect(Dst4D[1], ==, 6.0f);
+					DMibExpect(Dst4D[2], ==, 0.0f);
+					DMibExpect(Dst4D[3], ==, 0.0f);
+				}
+
+				{
+					DMibTestPath("LargerToSmaller");
+					CVec4Dfp32 Src4D(1.0f, 2.0f, 3.0f, 4.0f);
+					CVec2Dfp32 Dst2D(0.0f, 0.0f);
+					Dst2D = Src4D;
+					DMibExpect(Dst2D[0], ==, 1.0f);
+					DMibExpect(Dst2D[1], ==, 2.0f);
+				}
+			};
+
+			DMibTestSuite("CopyAndMove")
+			{
+				{
+					DMibTestPath("Copy");
+					CVec3Dfp32 Src(1.0f, 2.0f, 3.0f);
+					CVec3Dfp32 Dst(Src);
+					DMibExpect(Dst[0], ==, 1.0f);
+					DMibExpect(Dst[1], ==, 2.0f);
+					DMibExpect(Dst[2], ==, 3.0f);
+				}
+
+				{
+					DMibTestPath("Move");
+					CVec3Dfp32 Src(1.0f, 2.0f, 3.0f);
+					CVec3Dfp32 Dst(NMib::fg_Move(Src));
+					DMibExpect(Dst[0], ==, 1.0f);
+					DMibExpect(Dst[1], ==, 2.0f);
+					DMibExpect(Dst[2], ==, 3.0f);
+				}
+			};
+		}
+	};
+
+	DMibTestRegister(CVec_Tests, Malterlib::Numeric::Vector);
+}
